7b141f220b38e30d6cad085d09a6dc8857dcd8d4
unknown <you@example.com>
2022-01-01 20:45:50 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,73 @@
+import { Search } from "@material-ui/icons";
+import React from "react";
+import styled from "styled-components";
+
+// NAVBAR STYLE
+const Container = styled.div`
+    height: 60px;
+`;
+
+const Wrapper = styled.div`
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+const Left = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
+`;
+
+const Language = styled.span`
+    font-size: 14px;
+    cursor: pointer;
+`;
+const SearchContainer = styled.div`
+    border: 0.5px solid lightgray;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+`;
+
+const Input = styled.input`
+    border: none;
+`;
+
+const Center = styled.div`
+    flex: 1;
+    text-align: center;
+`;
+
+const Logo = styled.h1`
+    font-weight: bold;
+`;
+
+const Right = styled.div`
+    flex: 1;
+`;
+
+//end of style
+const Navbar = () => {
+    return (
+        <Container>
+            <Wrapper>
+                <Left>
+                    <Language>EN</Language>
+                    <SearchContainer>
+                        <Input></Input>
+                        <Search></Search>
+                    </SearchContainer>
+                </Left>
+
+                <Center>
+                    <Logo>Treats.</Logo>
+                </Center>
+                <Right>right</Right>
+            </Wrapper>
+        </Container>
+    );
+};
+
+export default Navbar;
