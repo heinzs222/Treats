5e8c8cbb2220df0887264c721f60aa51392b7945
unknown <you@example.com>
2022-01-02 17:41:59 +0100

    added newsletter

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Announcement from "../components/Announcement";
 import Categories from "../components/Categories";
 import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
 import Products from "../components/Products";
 
 import Slider from "../components/Slider";
@@ -14,6 +15,7 @@ function Home() {
             <Slider />
             <Categories />
             <Products />
+            <Newsletter />
         </div>
     );
 }
