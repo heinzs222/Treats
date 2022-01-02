c91f1ea78bf6707daf32603262d4ea5c7b86c0b1
unknown <you@example.com>
2022-01-02 15:34:53 +0100

    added products

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Announcement from "../components/Announcement";
 import Categories from "../components/Categories";
 import Navbar from "../components/Navbar";
+import Products from "../components/Products";
+
 import Slider from "../components/Slider";
 
 function Home() {
@@ -11,6 +13,7 @@ function Home() {
             <Navbar />
             <Slider />
             <Categories />
+            <Products />
         </div>
     );
 }
